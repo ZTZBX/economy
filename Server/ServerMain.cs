478cf3050e165c8b9fad0469a24edf4edf1a9caf
zabbix-byte <zabbix@ztrunk.space>
2023-08-23 13:09:48 +0200

    base

---
+++ Server/ServerMain.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace economy.Server
+{
+    public class ServerMain : BaseScript
+    {
+        public ServerMain()
+        {
+        }
+    }
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'bodacious'
+game 'gta5'
+
+files {
+    'Newtonsoft.Json.dll',
+    'static/index.css',
+    'static/index.js',
+    'static/index.html',
+    'static/*.png'
+}
+
+client_script 'Client/*.net.dll'
+server_script 'Server/*.net.dll'
+
+author 'zabbix-byte'
+version '1.0.0'
+description 'ztzbx economy system'
+
+
+dependencies {
+    "core-ztzbx",
+    "fivem-mysql",
+    "notification"
+}
+
+ui_pages {
+    'static/index.html'
+}
